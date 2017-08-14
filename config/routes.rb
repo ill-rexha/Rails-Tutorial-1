d81200806617a7886597ba0bc5e0adb401181fb8
yoshiki0000 <xxx.japan.0101@gmail.com>
2017-08-14 16:11:19 +0900

    Add New action to application_controller.rb file and routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # root 'controller_name#action_name'で指定する。
+  # root "application#hello"
+
+  root 'application#goodbye'
+
 end
